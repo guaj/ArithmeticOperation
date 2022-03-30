6d401ad92a454702e014e76c93fe361058b4bbfd
Jasmin <60197474+guaj@users.noreply.github.com>
2022-03-30 14:12:27 -0400

    Update ArithmeticOperationsTest.java
    Commented the ArithmeticOperationsTest.testSub method

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -17,7 +17,8 @@ public class ArithmeticOperationsTest {
 		assertEquals(expected, actual);
 
 	}
-    @Test
+   /*
+   @Test
     public void testSub()
     {
 
@@ -27,5 +28,6 @@ public class ArithmeticOperationsTest {
         assertEquals(expected, actual);
 
     }
+    */
 
 }
